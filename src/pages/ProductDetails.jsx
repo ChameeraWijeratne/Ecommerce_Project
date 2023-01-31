3ef95b288961a87318103fa943d5f3b206a71937
Chameera <wijerat>
2023-01-31 06:53:34 +0530

    8

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,7 +1,64 @@
 import React from "react";
 
+import {Container,Row,Col} from "reactstrap";
+import {useParams} from "react-router-dom";
+import products from "../assets/data/products";
+import Helmet from "../components/Helmet/Helmet";
+import CommonSection from "../components/UI/CommonSection";
+import '../styles/product-details.css';
+
 const ProductDetails = () => {
-    return <div>ProductDetails</div>;
+
+    const {id} = useParams()
+    const product = products.find(item => item.id === id)
+
+    const {imgUrl,productName, price, avgRating, review, description, shortDesc} = product
+
+    return (
+    <Helmet>
+        <CommonSection />
+
+        <section className="pt-0">
+            <Container>
+                <Row>
+                    <Col lg = '6'>
+                        <img className="image" src={imgUrl} alt=""/>
+                    </Col>
+                    <Col lg = '6'>
+                        <div className="product_details">
+                            <h2>{productName}</h2>
+                            <div className="product_rating d-flex align-items-center gap-5 mb-3">
+                                <div>
+                                    <span>
+                                        <i className="ri-star-s-fill"></i>
+                                    </span>
+                                    <span>
+                                        <i className="ri-star-s-fill"></i>
+                                    </span>
+                                    <span>
+                                        <i className="ri-star-s-fill"></i>
+                                    </span>
+                                    <span>
+                                        <i className="ri-star-s-fill"></i>
+                                    </span>
+                                    <span>
+                                        <i className="ri-star-half-s-line"></i>
+                                    </span>
+                                </div>
+                                <p>(<span>{avgRating}</span> ratings)</p>
+                            </div>
+
+                            <span className="product_price">${price}</span>
+                            <p className="mt-3">{shortDesc}</p>
+
+                            <button className="buy_btn">Add to Cart</button>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
+        </section>
+    </Helmet>
+    );
 };
 
 export default ProductDetails;
--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,7 +1,119 @@
-import React from "react";
+import React,{useState} from "react";
+
+import CommonSection from "../components/UI/CommonSection";
+import Helmet from "../components/Helmet/Helmet";
+import {Container,Row,Col} from "reactstrap";
+
+import '../styles/shop.css'
+
+import products from "../assets/data/products";
+import ProductsList from "../components/UI/ProductList";
 
 const Shop = () => {
-    return <div>Shop</div>;
+
+    const [productsData, setProductsData] = useState(products)
+
+    const handleFilter = e=>{
+        const filterValue = e.target.value
+            if(filterValue==='handbag'){
+                const filteredProducts = products.filter(item => item.category === 'handbag')
+                setProductsData(filteredProducts);
+            }
+
+            if(filterValue==='mobile'){
+                const filteredProducts = products.filter(item => item.category === 'mobile')
+                setProductsData(filteredProducts);
+            }
+
+            if(filterValue==='watch'){
+                const filteredProducts = products.filter(item => item.category === 'watch')
+                setProductsData(filteredProducts);
+            }
+
+            if(filterValue==='shoes'){
+                const filteredProducts = products.filter(item => item.category === 'shoes')
+                setProductsData(filteredProducts);
+            }
+
+            if(filterValue==='wireless'){
+                const filteredProducts = products.filter(item => item.category === 'wireless')
+                setProductsData(filteredProducts);
+            }
+
+            if(filterValue==='sunglass'){
+                const filteredProducts = products.filter(item => item.category === 'sunglass')
+                setProductsData(filteredProducts);
+            }
+
+
+    };
+
+    const handleSearch = e =>{
+        const searchTerm = e.target.value
+
+        const searchedProducts = products.filter(item => item.productName.toLowerCase().includes(
+            searchTerm.toLowerCase()))
+
+        setProductsData(searchedProducts)
+    }
+    return (
+    <Helmet title='Shop'>
+        <CommonSection title='Products' />
+            <section>
+                <Container>
+                    <Row>
+                        <Col lg='3' md='3'>
+                            <div className="filter_widget">
+                                <select onChange={handleFilter}>
+                                    <option>Filter By Category</option>
+                                    <option value="handbag">Handbag</option>
+                                    <option value="watch">Watch</option>
+                                    <option value="sunglass">Sunglass</option>
+                                    <option value="mobile">Mobile</option>
+                                    <option value="wireless">Headphone</option>
+                                    <option value="shoes">Shoes</option>
+                                </select>
+                            </div>
+                        </Col>
+                        <Col lg='3' md='3'>
+                            <div className="filter_widget">
+                                <select>
+                                    <option>Sort By</option>
+                                    <option value="ascending">Ascending</option>
+                                    <option value="descending">Descending</option>
+                                </select>
+                            </div>
+                        </Col>
+                        <Col lg='6' md='6'>
+                            <div className="search_box">
+                                <input
+                                    type="text"
+                                    placeholder="Search..."
+                                    onChange = {handleSearch}
+                                    />
+                                <span>
+                                    <i className="ri-search-line"></i>
+                                </span>
+                            </div>
+                        </Col>
+                    </Row>
+                </Container>
+            </section>
+        <section className="products">
+            <Container>
+                <Row>
+                    {
+                        productsData.length === 0? (
+                            <h1 className="text-center fs-4">No products are found!</h1>
+                        ) : (
+                            <ProductsList data={productsData} />
+                        )
+                    }
+                </Row>
+            </Container>
+        </section>
+    </Helmet>
+    );
 };
 
 export default Shop;
